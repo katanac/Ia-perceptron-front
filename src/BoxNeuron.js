e17c6cb7356be4a5ca93574dd6789b11e3835127
karen tatiana peña <karen.pena@infotrack.com.co>
2022-03-07 21:26:18 -0500

    cambio en front neurona

--- src/BoxNeuron.js
+++ src/BoxNeuron.js
@@ -21,36 +21,59 @@ const estadoInicialSegundoformulario = {
   error: 0
 }
 
-const urlCalcular = 'https://node-perceptron.herokuapp.com/calculate';
-const urlValidar = 'https://node-perceptron.herokuapp.com/validate'
+const estadoInicialResultadoCalculo = {
+  number: 0
+}
+
+
+const urlValidar = 'https://node-perceptron.herokuapp.com/calculate';
+const urlCalcular = 'https://node-perceptron.herokuapp.com/validate'
 
 function BoxNeuron() {
 
   const [estadoPrimerForm, setEstadoPrimerForm] = useState(estadoInicialPrimerFormulario);
   const [estadoSegundoForm, setEstadoSegundoForm] = useState(estadoInicialSegundoformulario);
+  const [resultadoCalculo, setResultadoCalculo] = useState(estadoInicialResultadoCalculo);
 
   const changeForm1 = (name, value) => {
     setEstadoPrimerForm({ ...estadoPrimerForm, [name]: value });
   };
 
   const changeForm2 = (name, value) => {
-    setEstadoPrimerForm({ ...estadoSegundoForm, [name]: value });
+    setEstadoSegundoForm({ ...estadoSegundoForm, [name]: value });
+  };
+
+  const changeResultCalculate = (name, value) => {
+    setEstadoSegundoForm({ ...resultadoCalculo, [name]: value });
   };
 
   const validarData = () => {
-
-    axios.post(urlCalcular, estadoPrimerForm).then((respuesta) => {
-      console.log(respuesta.data)
-      setEstadoSegundoForm(respuesta.data)
-      console.log("esta es la respuesta y estado 2", estadoInicialSegundoformulario)
+    axios.post(urlValidar, {
+      error: estadoPrimerForm.error === "" ? 0 : Number(estadoPrimerForm.error),
+      learning_factor: estadoPrimerForm.learning_factor === "" ? 0 : Number(estadoPrimerForm.learning_factor),
+      weight1: estadoPrimerForm.weight1 === "" ? 0 : Number(estadoPrimerForm.weight1),
+      weight2: estadoPrimerForm.weight2 === "" ? 0 : Number(estadoPrimerForm.weight2)
+    }).then((respuesta) => {
+      setEstadoSegundoForm({
+        w1: respuesta.data.w1,
+        w2: respuesta.data.w2,
+        counterAdjust: respuesta.data.counterAdjust,
+        error: respuesta.data.error
+      })
     })
   }
 
   const calculeteData = () => {
-
-    axios.post(urlCalcular, estadoSegundoForm).then((respuesta) => {
-      console.log(respuesta.data)
-      console.log("res2", estadoInicialSegundoformulario)
+    axios.post(urlCalcular, {
+      weight1: estadoSegundoForm.w1 === "" ? 0 : Number(estadoSegundoForm.w1),
+      weight2: estadoSegundoForm.w2 === "" ? 0 : Number(estadoSegundoForm.w2),
+      x1: estadoSegundoForm.x1 === "" ? 0 : Number(estadoSegundoForm.x1),
+      x2: estadoSegundoForm.x2 === "" ? 0 : Number(estadoSegundoForm.x2),
+      counterAdjust: estadoSegundoForm.counterAdjust === "" ? 0 : Number(estadoSegundoForm.counterAdjust),
+      error: estadoSegundoForm.error === "" ? 0 : Number(estadoSegundoForm.error)
+    }).then((respuesta) => {
+      console.log("respuesta 2", respuesta.data.number)
+      setResultadoCalculo({number:respuesta.data.number})
     })
   }
 
@@ -99,12 +122,12 @@ function BoxNeuron() {
           fontWeight: "700",
         }}
       >
-        <TextField value={estadoSegundoForm.w1} label='W1' />
-        <TextField value={estadoSegundoForm.w2} label='W2' />
-        <TextField value={estadoSegundoForm.counterAdjust} label='INTENTOS'  />
-        <TextField value={estadoSegundoForm.error} label='Error' />
-        <TextField value={estadoSegundoForm.x1} onChange={(v) => { changeForm2("x1", v.target.value) }} label='x1' />
-        <TextField value={estadoSegundoForm.x2} onChange={(v) => { changeForm2("x2", v.target.value) }} label='x2'  />
+        <TextField value={estadoSegundoForm.w1} label='W1' disabled />
+        <TextField value={estadoSegundoForm.w2} label='W2' disabled />
+        <TextField value={estadoSegundoForm.counterAdjust} label='INTENTOS' disabled />
+        <TextField value={estadoSegundoForm.error} label='Error' disabled />
+        <TextField onChange={(v) => { changeForm2("x1", v.target.value) }} label='x1' />
+        <TextField onChange={(v) => { changeForm2("x2", v.target.value) }} label='x2' />
         <Button
           style={{ marginRight: "10px" }}
           onClick={
@@ -112,6 +135,26 @@ function BoxNeuron() {
         >
           CALCULAR
         </Button>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          m: 1,
+          p: 1,
+          bgcolor: (theme) =>
+            theme.palette.mode === "dark" ? "#101010" : "#fff",
+          border: "2px solid",
+          borderColor: (theme) =>
+            theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+          borderRadius: 2,
+          fontSize: "0.875rem",
+          fontWeight: "700",
+        }}
+      >
+        <TextField value={
+          resultadoCalculo.number
+        } label='Numero calculado' disabled />
       </Box>
     </div>
   );
